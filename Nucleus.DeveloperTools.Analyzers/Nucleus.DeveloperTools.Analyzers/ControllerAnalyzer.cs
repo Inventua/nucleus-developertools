09b7b25a5ac8ba3ee5bf856068e05dc7b489e4a3
Anthony Glenwright <anthony.glenwright@inventua.com>
2023-08-18 08:59:26 +1000

    Added code to check that the current project is a nucleus extension by checking that package.xml exists.

--- Nucleus.DeveloperTools.Analyzers/Nucleus.DeveloperTools.Analyzers/ControllerAnalyzer.cs
+++ Nucleus.DeveloperTools.Analyzers/Nucleus.DeveloperTools.Analyzers/ControllerAnalyzer.cs
@@ -51,34 +51,37 @@ namespace Nucleus.DeveloperTools.Analyzers
     /// <param name="context"></param>
     private static void AnalyzeSymbols(SymbolAnalysisContext context)
     {
-      if (context.Symbol is INamedTypeSymbol symbol && symbol.DeclaredAccessibility.HasFlag(Accessibility.Public))
+      if (IsNucleusExtension(context.Symbol.Locations))
       {
-        // report a warning if there are controller classes which don't have an [Extension] attribute.
-        if (SymbolEqualityComparer.Default.Equals(symbol.BaseType, context.Compilation.GetTypeByMetadataName("Microsoft.AspNetCore.Mvc.Controller")))
+        if (context.Symbol is INamedTypeSymbol symbol && symbol.DeclaredAccessibility.HasFlag(Accessibility.Public))
         {
+          // report a warning if there are controller classes which don't have an [Extension] attribute.
+          if (SymbolEqualityComparer.Default.Equals(symbol.BaseType, context.Compilation.GetTypeByMetadataName("Microsoft.AspNetCore.Mvc.Controller")))
+          {
+            if (!symbol.GetAttributes()
+              .Where(attr => SymbolEqualityComparer.Default.Equals(attr.AttributeClass, context.Compilation.GetTypeByMetadataName("Nucleus.Abstractions.ExtensionAttribute")))
+              .Any())
+            {
+              context.ReportDiagnostic
+              (
+                Diagnostic.Create
+                (
+                  DiagnosticMessages.CONTROLLER_NO_EXTENSION_ATTRIBUTE,
+                  symbol.Locations.FirstOrDefault(),
+                  symbol.Name
+                )
+              );
+            }
+          }
+
+          // report an information message if the class has a method generally associated with an "admin" action (save/delete),
+          // and there is no [Authorize] attribute at the class level or on the method.
           if (!symbol.GetAttributes()
-            .Where(attr => SymbolEqualityComparer.Default.Equals(attr.AttributeClass, context.Compilation.GetTypeByMetadataName("Nucleus.Abstractions.ExtensionAttribute")))
+            .Where(attr => SymbolEqualityComparer.Default.Equals(attr.AttributeClass, context.Compilation.GetTypeByMetadataName("Microsoft.AspNetCore.Authorization.AuthorizeAttribute")))
             .Any())
           {
-            context.ReportDiagnostic
-            (
-              Diagnostic.Create
-              (
-                DiagnosticMessages.CONTROLLER_NO_EXTENSION_ATTRIBUTE,
-                symbol.Locations.FirstOrDefault(),
-                symbol.Name
-              )
-            );
+            CheckMethodAuthentication(context, symbol);
           }
-        }
-
-        // report an information message if the class has a method generally associated with an "admin" action (save/delete),
-        // and there is no [Authorize] attribute at the class level or on the method.
-        if (!symbol.GetAttributes()
-          .Where(attr => SymbolEqualityComparer.Default.Equals(attr.AttributeClass, context.Compilation.GetTypeByMetadataName("Microsoft.AspNetCore.Authorization.AuthorizeAttribute")))
-          .Any())
-        {
-          CheckMethodAuthentication(context, symbol);
         }
       }
     }
@@ -93,34 +96,64 @@ namespace Nucleus.DeveloperTools.Analyzers
     /// <param name="methodNamePart"></param>
     private static void CheckMethodAuthentication(SymbolAnalysisContext context, INamedTypeSymbol classSymbol)
     {
-      // Check if a method exists which matches any of the "well known" method name parts and is public
-      foreach (ISymbol methodSymbol in classSymbol.GetMembers()
-        .Where(member => member.DeclaredAccessibility.HasFlag(Accessibility.Public))
-        .Where(member => ContainsAny(member.Name, ADMIN_METHODS)))
+      if (IsNucleusExtension(classSymbol.Locations))
       {
-        // Check if the method has an attribute derived from HttpMethodAttribute (like [HttpPost])
-        if (methodSymbol.GetAttributes()
-            .Where(attribute => SymbolEqualityComparer.Default.Equals(attribute.AttributeClass.BaseType, context.Compilation.GetTypeByMetadataName("Microsoft.AspNetCore.Mvc.Routing.HttpMethodAttribute")))
-            .Any())
+        // Check if a method exists which matches any of the "well known" method name parts and is public
+        foreach (ISymbol methodSymbol in classSymbol.GetMembers()
+          .Where(member => member.DeclaredAccessibility.HasFlag(Accessibility.Public))
+          .Where(member => ContainsAny(member.Name, ADMIN_METHODS)))
         {
-          // Check if the method does not have an [Authorize] attribute
-          if (!methodSymbol.GetAttributes()
-            .Where(attribute => SymbolEqualityComparer.Default.Equals(attribute.AttributeClass, context.Compilation.GetTypeByMetadataName("Microsoft.AspNetCore.Authorization.AuthorizeAttribute")))
-            .Any())
+          // Check if the method has an attribute derived from HttpMethodAttribute (like [HttpPost])
+          if (methodSymbol.GetAttributes()
+              .Where(attribute => SymbolEqualityComparer.Default.Equals(attribute.AttributeClass.BaseType, context.Compilation.GetTypeByMetadataName("Microsoft.AspNetCore.Mvc.Routing.HttpMethodAttribute")))
+              .Any())
           {
-            context.ReportDiagnostic
-            (
-              Diagnostic.Create
+            // Check if the method does not have an [Authorize] attribute
+            if (!methodSymbol.GetAttributes()
+              .Where(attribute => SymbolEqualityComparer.Default.Equals(attribute.AttributeClass, context.Compilation.GetTypeByMetadataName("Microsoft.AspNetCore.Authorization.AuthorizeAttribute")))
+              .Any())
+            {
+              context.ReportDiagnostic
               (
-                DiagnosticMessages.CONTROLLER_ADMIN_NO_AUTHORIZE_ATTRIBUTE,
-                methodSymbol.Locations.FirstOrDefault(),
-                classSymbol.Name,
-                methodSymbol.Name
-              )
-            );
+                Diagnostic.Create
+                (
+                  DiagnosticMessages.CONTROLLER_ADMIN_NO_AUTHORIZE_ATTRIBUTE,
+                  methodSymbol.Locations.FirstOrDefault(),
+                  classSymbol.Name,
+                  methodSymbol.Name
+                )
+              );
+            }
           }
         }
       }
+    }
+
+    private static Boolean IsNucleusExtension(ImmutableArray<Location> locations)
+    {
+      foreach (Location location in locations)
+      {
+        if (FindPackageFile(System.IO.Path.GetDirectoryName(location.SourceTree.FilePath))) return true;
+      }
+      return false;
+    }
+
+    private static Boolean FindPackageFile(string path)
+    {
+#pragma warning disable RS1035 // Do not use APIs banned for analyzers
+      if (System.IO.Directory.EnumerateFiles(path, "package.xml").Any())
+      {
+        // found package.xml
+        return true;
+      }
+      else if (System.IO.Directory.EnumerateFiles(path, "*.csproj").Any())
+      {
+        // folder contains a project file, but not a package.xml, so this project is not a Nucleus extension
+        return false;
+      }
+#pragma warning restore RS1035 // Do not use APIs banned for analyzers
+
+      return FindPackageFile(System.IO.Path.GetDirectoryName(path));
     }
 
     private static Boolean ContainsAny(string part, IEnumerable<string> values)
